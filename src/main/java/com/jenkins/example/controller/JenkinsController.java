7848f0b4ddad198e3f8f9fcf59491043442bef20
wanglongbin <17325640141@163.com>
2024-07-08 21:33:14 +0800

    first commit

---
+++ src/main/java/com/jenkins/example/controller/JenkinsController.java
@@ -0,0 +1,22 @@
+package com.jenkins.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wanglb@belink.com
+ * @version V1.0
+ * @title
+ * @description
+ * @date 2024-07-08 21:15
+ */
+@RestController
+@RequestMapping("/jenkins")
+public class JenkinsController {
+
+    @GetMapping("/example")
+    public String example() {
+        return "hello jenkins";
+    }
+}
